7da5bd0732e403c7d6f55b19ddb5e149f0c9bcd9
AndreaRomano02 <andy.romano2002@gmail.com>
2023-07-03 15:11:32 +0200

    Add Pokemons Array from the store

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,29 @@
 <script>
+import axios from 'axios';
+import { store } from './assets/data/store.js'
+
+export default {
+  created() {
+    this.getPokemons();
+  },
+  data() {
+    return {
+      store,
+    }
+  }, methods: {
+    getPokemons() {
+      axios.get('https://41tyokboji.execute-api.eu-central-1.amazonaws.com/dev/api/v1/pokemons?eq[type1]=Electric&sort[number]=desc').then(
+        (res) => {
+          this.store.pokemons.push(res.data.docs)
+        }
+      )
+    },
+  },
+}
 </script>
 
 <template>
-  <h1 class="text-danger">ciao</h1>
+  <p v-for="pokemon in store.pokemons[0]">{{ pokemon.name }}</p>
 </template>
 
 <style lang="scss">
