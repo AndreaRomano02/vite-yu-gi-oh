f901534737eb162cb5e2d899e35c116aa5573d9d
AndreaRomano02 <andy.romano2002@gmail.com>
2023-07-03 15:16:25 +0200

    Fix push pokemons array

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,9 @@ export default {
     getPokemons() {
       axios.get('https://41tyokboji.execute-api.eu-central-1.amazonaws.com/dev/api/v1/pokemons?eq[type1]=Electric&sort[number]=desc').then(
         (res) => {
-          this.store.pokemons.push(res.data.docs)
+          res.data.docs.forEach(doc => {
+            this.store.pokemons.push(doc)
+          });
         }
       )
     },
@@ -23,7 +25,7 @@ export default {
 </script>
 
 <template>
-  <p v-for="pokemon in store.pokemons[0]">{{ pokemon.name }}</p>
+  <p v-for="pokemon in store.pokemons">{{ pokemon.name }}</p>
 </template>
 
 <style lang="scss">
