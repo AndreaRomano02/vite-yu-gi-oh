e3e64a7db221164c8c45131a063873a1d890a019
AndreaRomano02 <andy.romano2002@gmail.com>
2023-07-03 18:57:26 +0200

    Add the loading

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -5,29 +5,34 @@ import axios from 'axios';
 
 // LOGIC
 export default {
-
+  props: {
+    pokemons: Array,
+  },
   created() {
 
     // Riempio l'array dei pokemons al montare della pagina
     this.getPokemons();
+    this.isLoading = true;
   },
   data() {
     return {
+      isLoading: false,
     }
-  },
-  props: {
-    pokemons: Array
   },
   methods: {
     getPokemons() {
-      axios.get('https://41tyokboji.execute-api.eu-central-1.amazonaws.com/dev/api/v1/pokemons?eq[type1]=Electric&sort[number]=desc').then(
-        (res) => {
+      axios.get('https://41tyokboji.execute-api.eu-central-1.amazonaws.com/dev/api/v1/pokemons?eq[type1]=Electric&sort[number]=desc')
+        .then(res => {
           res.data.docs.forEach(doc => {
             this.pokemons.push(doc);
-
-          });
-        }
-      )
+          })
+        })
+        .catch(err => {
+          console.error(err.message)
+        })
+        .then(() => {
+          this.isLoading = false
+        });
     },
   },
   computed: {
@@ -39,7 +44,8 @@ export default {
 </script>
 
 <template>
-  <div class="container py-5">
+  <h1 v-if="isLoading" class="loading">LOADING...</h1>
+  <div v-else class="container py-5">
     <h1 class="text-center mb-5">POKEDEX</h1>
     <div class="row row-cols-3 g-4">
       <div v-for="pokemon in orderedPokemons" :key="pokemon.number" class="col">
@@ -59,6 +65,19 @@ export default {
 </template>
 
 <style>
+.loading {
+  position: fixed;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  background-color: rgba(0, 0, 0, 0.5);
+  color: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
 .card-img-top {
   height: 300px;
   border-bottom: 1px solid lightgray;
