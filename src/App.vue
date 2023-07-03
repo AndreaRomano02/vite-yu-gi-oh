063a64230641ed87aea8f674c708b67efe511f78
AndreaRomano02 <andy.romano2002@gmail.com>
2023-07-03 13:48:51 +0200

    Add bootsrap, axios and sass

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <script>
 </script>
 
-<template></template>
+<template>
+  <h1 class="text-danger">ciao</h1>
+</template>
 
-<style></style>
+<style lang="scss">
+@use './assets/scss/style.scss' as *;
+</style>
